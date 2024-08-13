368265fe7e41d9c8a3a19114656cfc1dc49d4a61
Debbie Hoffpauir <d.j.hoffpauir@gmail.com>
2024-08-13 14:53:45 -0500

    Update streamlit_app.py
    remove extra text

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,4 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
