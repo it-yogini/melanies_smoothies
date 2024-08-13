c346c7640df869fd7157133f28c4e2843aed1eae
Debbie Hoffpauir <d.j.hoffpauir@gmail.com>
2024-08-13 14:52:30 -0500

    Update streamlit_app.py
    bad colon

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/: +fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
