353a2bf56d209a8a2809896d1f8d22d1fca2befa
Debbie Hoffpauir <d.j.hoffpauir@gmail.com>
2024-08-13 14:39:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,5 @@ if time_to_insert:
 #fruityvice nutrition information import
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
