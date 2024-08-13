90a3b38ad0acdd6e6b7a8a20e7c9cfe090defd5d
Debbie Hoffpauir <d.j.hoffpauir@gmail.com>
2024-08-13 14:51:28 -0500

    Update streamlit_app.py
    adding header to fruit nutrition

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/: +fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
