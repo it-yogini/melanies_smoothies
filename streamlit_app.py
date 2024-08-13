330d8b13f72e962f4a61687d612b2cac06cb3f2f
Debbie Hoffpauir <d.j.hoffpauir@gmail.com>
2024-08-13 16:01:38 -0500

    Update streamlit_app.py
    use search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ session = cnx.session()
 #session = get_active_session()
 
 name_on_order= st.text_input('Name on Smoothie: ')
-
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'), col('Search_on'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -41,10 +41,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
