fa5af8716c9b7dadbe8fab1badd53415141c9fbc
Debbie Hoffpauir <d.j.hoffpauir@gmail.com>
2024-08-13 14:45:52 -0500

    Update streamlit_app.py
    Adding nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
+
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -32,6 +34,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -48,8 +52,7 @@ if time_to_insert:
     
     st.success('Your Smoothie is ordered!', icon="✅")
 
-#fruityvice nutrition information import
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
