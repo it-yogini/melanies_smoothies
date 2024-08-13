8d1796d76fd7df46d18265dd210d98541aec7e5e
Debbie Hoffpauir <d.j.hoffpauir@gmail.com>
2024-08-13 15:39:22 -0500

    Update streamlit_app.py
    ADD LOCATOR

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 
 #convert SN DF to PD DF
 pd_df - my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
         'Choose up to 5 ingredients:'
@@ -40,6 +40,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
