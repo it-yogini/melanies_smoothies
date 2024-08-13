f8a8fcf8d002f36605eac5b485e7ba793a226590
Debbie Hoffpauir <d.j.hoffpauir@gmail.com>
2024-08-13 15:40:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-import Pandas as pd
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
